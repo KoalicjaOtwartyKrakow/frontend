87b49157a1aa213391eb8ff7072aa7a092f445ef
Jakub Argasiński <jakub@argasinski.eu>
2022-02-27 10:35:32 +0100

    Fix route path

--- src/constants/Routes.js
+++ src/constants/Routes.js
@@ -2,7 +2,7 @@ class Routes {
     static ROOT = '/';
     static APARTMENTS = '/apartments';
     static APARTMENTS_CREATE = Routes.APARTMENTS + '/create';
-    static APARTMENTS_EDIT = Routes.APARTMENTS + '/:id/edit';
+    static APARTMENTS_EDIT = Routes.APARTMENTS + '/:apartmentId/edit';
 }
 
 export default Routes;
