059cb9d54faeacbda7584cacc142de1eead4d227
klaglu <46421490+klaglu@users.noreply.github.com>
2022-03-11 19:38:53 +0100

    #55 Make Jumbotron a clickable area which navigates to Dashboard (#64)
    Co-authored-by: Jakub Argasiński <jakub@argasinski.eu>

--- src/components/AuthenticatedContainer.jsx
+++ src/components/AuthenticatedContainer.jsx
@@ -4,13 +4,14 @@ import { withRouter } from "react-router-dom";
 import { Notifications } from "services/Notifications";
 import { ToastProvider } from "react-toast-notifications";
 import { useTranslation } from "react-i18next";
-
+import { useHistory } from "react-router-dom";
 import { Routes } from "constants/Routes";
 import Jumbotron from "components/atoms/compat/Jumbotron";
 import AuthenticatedNavbar from "components/navbar/AuthenticatedNavbar";
 import { emptyFn } from "shared/utils";
 
-const AuthenticatedContainer = ({ children, history }) => {
+const AuthenticatedContainer = ({ children }) => {
+    const history = useHistory();
     const onJumbotronClick = () => {
         const path = Routes.ROOT;
         history.push(path);
@@ -21,7 +22,7 @@ const AuthenticatedContainer = ({ children, history }) => {
     return (
         <ToastProvider autoDismiss={Notifications.toastAutoDismiss}>
             <AuthenticatedNavbar onLogout={emptyFn} />
-            <Jumbotron onClick={onJumbotronClick} className="pointer">
+            <Jumbotron onClick={onJumbotronClick}>
                 <Container>
                     <h1 className="display-6">#KoalicjaOtwartyKraków</h1>
                     <p className="lead">
@@ -31,7 +32,7 @@ const AuthenticatedContainer = ({ children, history }) => {
             </Jumbotron>
             <Container>
                 <div className="mt-3 mb-3 text-muted">
-                    Dashboard > Breadcrumbs here...
+                    Dashboard {">"} Breadcrumbs here...
                 </div>
                 {children}
             </Container>
