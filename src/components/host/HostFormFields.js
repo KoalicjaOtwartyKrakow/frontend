7307d7d8c74b128ce02c91cd28df0557734731fb
Piotr Stolarek <stolarek.piotr@gmail.com>
2022-03-12 14:23:06 +0100

    #29 [Hosts] Host extended information section

---
+++ src/components/host/HostFormFields.js
@@ -0,0 +1,10 @@
+class HostFormFields {
+    // TODO: to be aligned with API later
+    static LGBT_FRIENDLY = "lgbtFriendly";
+    static ACCEPTS_FROM_ANY_COUNTRY = "acceptsFromAnyCountry";
+    static ACCEPTS_GUEST_WITH_DISABILITIES = "acceptsGuestWithDisabilities";
+}
+
+const hostFormFields = new HostFormFields();
+
+export { hostFormFields, HostFormFields };
