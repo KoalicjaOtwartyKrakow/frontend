2f32c56b39f4fe012aed4f80b114399f78f27b31
Piotr Stolarek <stolarek.piotr@gmail.com>
2022-03-14 14:34:03 +0100

    #60 [Guest] Create Guest Form validation schema

---
+++ src/components/guest/GuestFormFields.js
@@ -0,0 +1,30 @@
+class GuestFormFields {
+    static ADULT_FEMALE_COUNT = "adultFemaleCount";
+    static ADULT_MALE_COUNT = "adultMaleCount";
+    static CHILDREN = "children";
+    static DESIRED_DESTINATION = "desiredDestination";
+    static EMAIL = "email";
+    static FINANCE_STATUS = "financeStatus";
+    static FOOD_ALLERGIES = "foodAllergies";
+    static FULL_NAME = "fullName";
+    static GLUTEN_FREE_DIET = "glutenFreeDiet";
+    static HAVE_PETS = "havePets";
+    static HOW_LONG_TO_STAY = "howLongToStay";
+    static LACTOSE_FREE_DIET = "lactoseFreeDiet";
+    static MEAT_FREE_DIET = "meatFreeDiet";
+    static PEOPLE_IN_GROUP = "peopleInGroup";
+    static PETS_DESCRIPTION = "petsDescription";
+    static PHONE_NUMBER = "phoneNumber";
+    static PRIORITY_DATE = "priorityDate";
+    static PRIORITY_STATUS = "priorityStatus";
+    static SPECIAL_NEEDS = "specialNeeds";
+    static VERIFICATION_STATUS = "verificationStatus";
+    static ID = "id";
+
+    // children array object
+    static CHILD_AGE = "age";
+}
+
+const guestFormFields = new GuestFormFields();
+
+export { guestFormFields, GuestFormFields };
