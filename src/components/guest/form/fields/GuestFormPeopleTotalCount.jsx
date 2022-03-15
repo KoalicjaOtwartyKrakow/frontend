2b7da0376f94e2224a41ec8cc2053b6f45d6b95a
Piotr Stolarek <stolarek.piotr@gmail.com>
2022-03-15 13:18:04 +0100

    Remove automatic total people count

--- src/components/guest/form/fields/GuestFormPeopleTotalCount.jsx
+++ src/components/guest/form/fields/GuestFormPeopleTotalCount.jsx
@@ -1,37 +1,40 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { FormGroup, Label } from "reactstrap";
-import { Field, useField, useFormikContext } from "formik";
+import { Field } from "formik";
 import { useTranslation } from "react-i18next";
 
 import { GuestFormFields } from "components/guest/GuestFormFields";
 import FormInput from "components/atoms/form/FormInput";
 
 const GuestFormPeopleTotalCount = (props) => {
-    const {
-        values: { peopleFemaleCount, peopleMaleCount, children },
-        touched,
-        setFieldValue,
-    } = useFormikContext();
-    const [field] = useField({
-        ...props,
-        name: GuestFormFields.PEOPLE_TOTAL_COUNT,
-    });
-    const fieldId = field.name;
+    const fieldId = GuestFormFields.PEOPLE_TOTAL_COUNT;
 
-    useEffect(() => {
-        setFieldValue(
-            field.name,
-            (peopleFemaleCount || 0) +
-                (peopleMaleCount || 0) +
-                (children.length || 0)
-        );
-    }, [
-        peopleFemaleCount,
-        peopleMaleCount,
-        children,
-        setFieldValue,
-        field.name,
-    ]);
+    // **********************************************
+    // Code for handling automatic total people count
+    // **********************************************
+    // const {
+    //     values: { peopleFemaleCount, peopleMaleCount, children },
+    //     touched,
+    //     setFieldValue,
+    // } = useFormikContext();
+    // const [field] = useField({
+    //     ...props,
+    //     name: GuestFormFields.PEOPLE_TOTAL_COUNT,
+    // });
+    // useEffect(() => {
+    //     setFieldValue(
+    //         field.name,
+    //         (peopleFemaleCount || 0) +
+    //             (peopleMaleCount || 0) +
+    //             (children.length || 0)
+    //     );
+    // }, [
+    //     peopleFemaleCount,
+    //     peopleMaleCount,
+    //     children,
+    //     setFieldValue,
+    //     field.name,
+    // ]);
 
     const { t } = useTranslation(["guest"]);
     return (
@@ -45,7 +48,6 @@ const GuestFormPeopleTotalCount = (props) => {
                 name={fieldId}
                 placeholder={0}
                 type="number"
-                disabled
             />
         </FormGroup>
     );
