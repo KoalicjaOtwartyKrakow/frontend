524994625595ffe1a4e7a852cd41f2fdcdd7d15e
klaglu <46421490+klaglu@users.noreply.github.com>
2022-03-15 10:32:16 +0100

    Update GuestSearchItem.jsx

--- src/components/guest/form/GuestSearchItem.jsx
+++ src/components/guest/form/GuestSearchItem.jsx
@@ -3,14 +3,7 @@ import "components/guests/GuestListItem.sass";
 import { GuestContext } from "components/guest/GuestContext";
 import GuestItemFullName from "components/guests/item/GuestItemFullName";
 import GuestItemPhone from "components/guests/item/GuestItemPhone";
-/**
- *
- * @param {Guest} guest
- * @param {function} onEdit
- * @param {function} onRemove
- * @returns {JSX.Element}
- * @constructor
- */
+
 const GuestSearchItem = ({ guest }) => {
     return (
         <GuestContext.Provider value={guest}>
