2dbc050848efd0daaa258e71ea2da519437f88c4
Jakub Argasiński <jakub@argasinski.eu>
2022-03-19 15:14:55 +0100

    [Tech] (Hopefully) Fix issues with image URLs

--- src/components/atoms/compat/Jumbotron.jsx
+++ src/components/atoms/compat/Jumbotron.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Jumbotron = ({ children, onClick }) => {
     return (
-        <header onClick={onClick} className="bg-light py-3 pointer">
+        <header onClick={onClick} className="bg-light py-2 py-lg-3 pointer">
             {children}
         </header>
     );
