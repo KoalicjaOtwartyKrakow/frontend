55b10c35e0e506863c6d85b0f78b809951a8922f
Michał Łazowik <mlazowik@gmail.com>
2022-03-20 02:33:05 +0100

    Fix status select localization in the host form

--- src/components/host/form/fields/HostFormStatus.jsx
+++ src/components/host/form/fields/HostFormStatus.jsx
@@ -2,22 +2,42 @@ import React from "react";
 import { FormGroup } from "reactstrap";
 import { Field } from "formik";
 import { useTranslation } from "react-i18next";
+
 import { HostFormFields } from "components/host/HostFormFields";
-import { FormItemsHostStatus } from "components/molecules/form/FormItemsHostStatus";
+import FormSelect from "components/atoms/form/FormSelect";
+import { HostStatus } from "models/constants/HostStatus";
 import FormLabel from "components/atoms/form/FormLabel";
 
 const HostFormStatus = (props) => {
     const fieldId = HostFormFields.STATUS;
+
     const { t } = useTranslation(["host"]);
+
+    const items = [
+        {
+            id: HostStatus.CREATED,
+            name: t("host:status.created"),
+        },
+        {
+            id: HostStatus.VERIFIED,
+            name: t("host:status.verified"),
+        },
+        {
+            id: HostStatus.REJECTED,
+            name: t("host:status.rejected"),
+        },
+    ];
+
     return (
         <FormGroup>
             <FormLabel for={fieldId} className="required">
                 {t("host:form.label.hostStatus")}
             </FormLabel>
             <Field
-                component={FormItemsHostStatus}
+                component={FormSelect}
                 id={fieldId}
                 name={fieldId}
+                items={items}
             />
         </FormGroup>
     );
