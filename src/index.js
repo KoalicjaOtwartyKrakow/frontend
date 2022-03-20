c73a0939753975b924ff38cd3bf526836bf041a9
Michał Łazowik <mlazowik@gmail.com>
2022-03-20 12:28:12 +0100

    Add sentry (#150)
    Closes: #149

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import "core-js/proposals/reflect-metadata";
 import "styles/bootstrap-pre-custom.sass";
 import "styles/bootstrap-post-custom.sass";
@@ -13,6 +15,15 @@ import { appConfig } from "constants/AppConfig";
 import "./i18n";
 // import reportWebVitals from './reportWebVitals';
 import "mocks/index";
+
+Sentry.init({
+    dsn: appConfig.sentryDsn,
+    integrations: [new BrowserTracing()],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: appConfig.sentryTracesSampleRate,
+});
 
 axios.defaults.baseURL = baseUrl;
 axios.defaults.timeout = timeout;
