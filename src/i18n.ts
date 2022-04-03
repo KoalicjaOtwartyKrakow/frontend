e4a395f81ad8ec00eb30ede88568e858fe503587
Jakub Argasiński <jakub@argasinski.eu>
2022-04-03 23:02:24 +0200

    #208 [Tech] WhyDidYouRender integration
    Add why-did-you-render with default setup (i.e. pure components).
    Remove leftovers from JS -> TS migration (lodash-es, @types/lodash-es)
    Remove react-app-rewired and customize-cra. Decorators are now handled in babel.config.js.
    Remove intl-pluralrules. It is not required for any modern browser.

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,4 @@
 import i18next from "i18next";
-import "intl-pluralrules";
 import I18nextHttpBackend from "i18next-http-backend";
 import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
