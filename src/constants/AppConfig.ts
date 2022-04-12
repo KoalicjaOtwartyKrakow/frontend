d60beca1b6a81e6c4f389752da8b29e79cd72dcb
Bohdan <qwebeck5@gmail.com>
2022-04-12 04:12:10 +0200

    Solution with token validation and automatic sign out

--- src/constants/AppConfig.ts
+++ src/constants/AppConfig.ts
@@ -14,4 +14,5 @@ export const appConfig = Object.freeze({
     sentryTracesSampleRate: parseFloat(process.env.REACT_APP_SENTRY_TRACES_SAMPLE_RATE || "1.0"),
     defaultTimeout: Number(process.env.REACT_APP_KOKON_API_TIMEOUT || 10000),
     useMocks: process.env.REACT_APP_KOKON_API_USE_MOCKS === "true" && process.env.NODE_ENV === "development",
+    checkTokenInterval: Number(process.env.REACT_APP_CHECK_TOKEN_INTERVAL || 10000),
 });
